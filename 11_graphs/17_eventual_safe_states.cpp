dd4a00657493778a382c1fbfd03b80df4e0cd494
Devanshi Bhatt <dbhatt2310@gmail.com>
2026-06-06 15:01:55 +0530

    Eventual safe states - graph ques

---
+++ 11_graphs/17_eventual_safe_states.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <queue>
+#define ll long long
+using namespace std;
+
+bool dfs(int st, vector<vector<int>> &adj_list, vector<int> &visited, vector<int> &path_visited, vector<int> &check) {
+    visited[st] = 1;
+    path_visited[st] = 1;
+    check[st] = 0;
+
+    for(int node : adj_list[st]) {
+        if(!visited[st] && dfs(node,adj_list,visited,path_visited,check)) {
+            return true;
+        }
+        else if(path_visited[node]) return true;
+    }
+
+    check[st] = 1;
+    path_visited[st] = 0;
+    return false;
+}
+
+vector<int> safe_nodes(int start, vector<vector<int>> &adj_list, vector<int> &visited, vector<int> &path_visited) {
+    int n = visited.size()-1;
+    vector<int> safenodes;
+    vector<int> check(n+1,0);
+
+    for(int i=1; i<=n; i++) {
+        if(!visited[i]) {
+            dfs(start,adj_list,visited,path_visited,check);
+        }
+    }
+
+    for(int i=1; i<=n; i++) {
+        if(check[i]) safenodes.push_back(i);
+    }
+}
