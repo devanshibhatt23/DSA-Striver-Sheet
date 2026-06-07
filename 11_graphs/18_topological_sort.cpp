342af9421aa017d60f7e31b2a37482e0763282c9
Devanshi Bhatt <dbhatt2310@gmail.com>
2026-06-07 15:35:24 +0530

    Topological sort - graph

---
+++ 11_graphs/18_topological_sort.cpp
@@ -0,0 +1,51 @@
+/* topological sort 
+- only in directed acyclic graph
+- if there is an edge from u to v in the graph (u -> v), then u appears before v in the sorted arr
+*/
+
+#include <bits/stdc++.h>
+#include <stack>
+using namespace std;
+
+vector<vector<int>> adjacency_list(int n, int m) {
+    vector<vector<int>> arr(n+1, vector<int> (0));
+
+    while(m--) {
+        int x,y;
+        cin >> x >> y;
+
+        arr[x].push_back(y);
+        arr[y].push_back(x);
+    }
+
+    return arr;
+}
+
+void dfs(int start, vector<vector<int>> &adj_list, vector<int> &visited, stack<int> &st) {
+    visited[start] = 1;
+
+    for(int node : adj_list[start]) {
+        if(!visited[node]) dfs(node,adj_list,visited,st);
+    }
+
+    st.push(start);
+}
+
+vector<int> topologicalSort(vector<vector<int>> &adj_list) {
+    int n = adj_list.size();
+
+    vector<int> visited(n+1,0);
+    vector<int> sorted;
+    stack<int> st;
+
+    for(int i=1; i<=n; i++) {
+        if(!visited[i]) dfs(i,adj_list,visited,st);
+    }
+
+    while(!st.empty()) {
+        sorted.push_back(st.top());
+        st.pop();
+    }
+
+    return sorted;
+}
