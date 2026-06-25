39988c6dc03f6b3a2046ef09fa2fcaa83cb54a55
Devanshi Bhatt <dbhatt2310@gmail.com>
2026-06-25 16:26:12 +0530

    House robbers - dp ques

---
+++ 12_dynamic_programming/05_house_robber.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int memoization(int idx, vector<int> &nums, vector<int> &dp) {
+    if(idx == 0) {
+        dp[0] = nums[0];
+        return nums[0];
+    }
+
+    if(idx < 0) return 0;
+
+    if(dp[idx] != -1) return dp[idx];
+
+    int pick = nums[idx] + memoization(idx-2,nums,dp);
+    int not_pick = 0 + memoization(idx-1,nums,dp);
+
+    return dp[idx] = max(pick,not_pick);
+}
+
+int tabulation(int n, vector<int> &nums, vector<int> &dp) {
+    dp[0] = nums[0];
+    if(1 < n) {
+        dp[1] = max(nums[1],dp[0]);
+    }
+
+    for(int i=2; i<n; i++) {
+        int pick = nums[i] + dp[i-2];
+        int not_pick = 0 + dp[i-1];
+
+        dp[i] = max(pick,not_pick);
+    }
+
+    return dp[n-1];
+}
+
+int space_optimization(int n, vector<int> &nums) {
+    int prev2 = nums[0];
+    if(n == 1) return prev2;
+
+    int prev = max(nums[1],prev2);
+
+    for(int i=2; i<n; i++) {
+        int pick = nums[i] + prev2;
+        int not_pick = 0 + prev;
+
+        prev2 = prev;
+        prev = max(pick,not_pick);
+    }
+
+    return prev;
+}
+
+int rob(vector<int> &nums) {
+    int n = nums.size();
+
+    return space_optimization(n,nums);
+}
